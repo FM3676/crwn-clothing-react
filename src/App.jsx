6552a9f58de38945cb57b69bd8e97deeb83d9e92
§ <1309346392@qq.com>
2022-08-31 00:29:08 +0800

    Separate the directory to the hhomePage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import Directory from "./components/directory/directory.component";
+import { Route, Routes } from 'react-router'
+import Home from './routes/home/home.compon/home.component'
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
+  )
+}
 
-  return <Directory categories={categories} />;
-};
-
-export default App;
+export default App
+ 
