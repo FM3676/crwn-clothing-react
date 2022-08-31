f5702117e8a7c12c6fe88d337667f26411ac3bca
§ <1309346392@qq.com>
2022-08-31 19:11:39 +0800

    Finish base nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from 'react-router'
 import Home from './routes/home/home.compon/home.component'
+import Navigation from './routes/navigation/navigation.component'
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
     </Routes>
   )
 }
 
 export default App
- 
