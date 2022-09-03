30e1ac970fc5d1c0d52b45e8ed081d50f72432a8
§ <1309346392@qq.com>
2022-09-03 15:56:22 +0800

    Finish Add to cart functionality.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import "./cart-dropdown.styles.scss";
 
-import Button from '../button/button.component'
+import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
-    return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items"></div>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    )
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
 };
 
 export default CartDropdown;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
 import "./product-card.styles.scss";
 
 import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => {
+    addItemToCart(product);
+  };
 
   return (
     <div className="product-card-container">
@@ -12,7 +19,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button onClick={addProductToCart} buttonType="inverted">
+        Add to card
+      </Button>
     </div>
   );
 };
