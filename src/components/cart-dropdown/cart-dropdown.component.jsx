282efe092f8c300789333a2ea7fbaef4d748ff96
§ <1309346392@qq.com>
2022-09-06 12:03:58 +0800

    Use Styled Component on Cart Icon.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -19,7 +19,7 @@ const CartDropdown = () => {
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
-          <EmptyMessage>Your cart is empty</EmptyMessage>
+          <EmptyMessage>Your cart is empty</EmptyMessage   >
         )}
       </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
