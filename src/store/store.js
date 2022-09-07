8043d6e7fefc537c2254663bc2505812a828a72d
§ <1309346392@qq.com>
2022-09-08 01:07:13 +0800

    Create own middleware fun logger

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,25 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+// Create your own middleware Fun
+/*      Currying Funcion 
+    const curryFuc = a => (b, c) => a + b - c;
+    const wit3 = curryFun(3);
+    with3(2, 4) -----> 3 + 2 - 4
+*/
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) return next(action);
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  // It will only get us the new state if it's updated once it's run through all of the reducers with the action.
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
 // root-reducer
 
 // Create middleware(run before an action hits the reducer), Whnerever dispatch an action before that action hits the reducers, it hits the middleware first.
@@ -14,9 +33,9 @@ const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // pass enhancers as thrid argument, we can have different types of enhancers, middleWare is primary one.
 export const store = createStore(
-    rootReducer, // necessary
-    undefined, // add any additional default states
-    composedEnhancers
+  rootReducer, // necessary
+  undefined, // add any additional default states
+  composedEnhancers
 );
 
 // We pass them as this compose enhancers thrid argument, it enhances our store, the middleware enhance our store
