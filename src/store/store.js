e996440e755070be24bf7f6046f8ff219b2be183
§ <1309346392@qq.com>
2022-09-08 15:50:46 +0800

    Setting and use Redux DevTools

--- src/store/store.js
+++ src/store/store.js
@@ -5,25 +5,6 @@ import persistStore from "redux-persist/es/persistStore";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
-// Create your own middleware Fun
-/*      Currying Funcion 
-    const curryFuc = a => (b, c) => a + b - c;
-    const wit3 = curryFun(3);
-    with3(2, 4) -----> 3 + 2 - 4
-*/
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) return next(action);
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  // It will only get us the new state if it's updated once it's run through all of the reducers with the action.
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
-
 const persistConfig = {
   key: "root",
   storage,
@@ -35,12 +16,21 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 // root-reducer
 
 // Create middleware(run before an action hits the reducer), Whnerever dispatch an action before that action hits the reducers, it hits the middleware first.
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+// Enable Redux DevTools
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 // In order for these middleware to actually work, we have to call applyMiddleware()
 // "What we can do is we can say, these middleware are actually something like enhancers"
 // Compose is a functional programming concept, it's essentially a way for us to pass multiple functions left to right.
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 // pass enhancers as thrid argument, we can have different types of enhancers, middleWare is primary one.
 export const store = createStore(
